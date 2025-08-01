96f6c4a884de7c23c4ea027fe386fac69f0a0dfa
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2025-08-01 15:13:25 +0200

    Added settings screen

--- scenes/ui/ui_manager.gd
+++ scenes/ui/ui_manager.gd
@@ -4,16 +4,22 @@ extends CanvasLayer
 @onready var loading: Control = $Loading
 @onready var pause: Control = $Pause
 @onready var gui: Control = $GUI
+@onready var settings: Control = $Settings
+@onready var bg: TextureRect = $Circus
 
 func hide_all() -> void:
 	main_menu.visible = false
 	loading.visible = false
 	pause.visible = false
+	gui.visible = false
+	settings.visible = false
+	bg.visible = false
 	GlobalSignals.pause.emit(pause.visible)
 
 func show_menu() -> void:
 	hide_all()
 	main_menu.visible = true
+	bg.visible = true
 
 func show_gui() -> void:
 	hide_all()
@@ -32,6 +38,11 @@ func show_game_over() -> void:
 	hide_all()
 	#game_over.visible = true
 
+func show_settings() -> void:
+	hide_all()
+	settings.visible = true
+	bg.visible = true
+
 func _on_play_pressed() -> void:
 	GlobalSignals.new_game.emit()
 
@@ -39,9 +50,14 @@ func _on_continue_pressed() -> void:
 	show_gui()
 
 func _on_main_menu_pressed() -> void:
-	hide_all()
+	show_menu()
 	GlobalSignals.level_closed.emit()
-	main_menu.visible = true
 
 func _on_quit_pressed() -> void:
 	GlobalSignals.quit.emit()
+
+func _on_settings_pressed() -> void:
+	show_settings()
+
+func _on_back_pressed() -> void:
+	show_menu()
